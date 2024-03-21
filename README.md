ff30022a31b134e11de20c74310ee7d31e3a58b8
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-03-21 08:01:38 +0200

    feat(experiment1/): Location description now updated by user action

---
+++ README.md
@@ -0,0 +1,6 @@
+# Experiments in LLM fiction generation
+
+| Experiment | Aim 
+|------------|------------------
+| 1          | Show that even a small language model can produce high quality stories, by manually handling state and constraining the model
+| 2          | Force the LLM to construct a short story which includes specific literature elements (https://thewritepractice.com/elements-of-a-short-story/)
--- experiment1/src/game_state/update_game_state.py
+++ experiment1/src/game_state/update_game_state.py
@@ -6,6 +6,7 @@ from src.prompting.prompt_generator import PromptGenerator
 
 logger = logging.getLogger(__name__)
 
+
 def update_game_state(
     current_location: Location, prompt_generator: PromptGenerator, llm: Llm
 ):
@@ -27,12 +28,12 @@ def update_game_state(
     """
     )
 
+    print("The following options are available to you:")
     for idx, location in enumerate(current_location.adjacent_locations):
-        print(
-            f"""--Option [{idx}]--
-Travel to [{location.name}]
-"""
-        )
+        print(f'    Enter "{idx}" to travel to new location [{location.name}]')
+    print('    Enter "exit" to leave the game')
+    print("    Any other text you submit will be interpreted as your action")
+    print('        e.g. "I close my eyes and shout as loud as I can"')
 
     player_choice: str = input("Please choose an action: ")
     if player_choice.isdigit():
@@ -45,7 +46,16 @@ Travel to [{location.name}]
             user_action_description=player_choice,
         )
         logger.debug('Prompt:\n"%s"', prompt)
-        print(llm.respond(prompt))
+        location_response_to_user_action: str = llm.respond(prompt)
+        print(location_response_to_user_action)
         _: str = input("Please press enter to continue")
+        prompt: str = prompt_generator.user_action_updates_location_description(
+            location_name=current_location.name,
+            location_description=current_location.description,
+            user_action_description=player_choice,
+            location_response_to_user_action=location_response_to_user_action,
+        )
+        logger.debug('Prompt:\n"%s"', prompt)
+        current_location.description = llm.respond(prompt)
 
     return current_location, player_choice
